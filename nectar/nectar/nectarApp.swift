a7de043637e43c2980e0a7ce47534768c0ec1efa
Vladislav <vladislav@Vladislav-MacBook-Pro.local>
2023-05-13 23:30:03 +0300

    First commit

---
+++ nectar/nectar/nectarApp.swift
@@ -0,0 +1,17 @@
+//
+//  nectarApp.swift
+//  nectar
+//
+//  Created by Vladislav on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct nectarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
